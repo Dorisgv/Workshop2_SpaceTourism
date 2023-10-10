6c5e1ab7e748413238629e5d0942b7d646d3102e
ASernaLopez <alejo21625@gmail.com>
2023-10-09 21:15:54 -0500

    haciendo cambios

--- src/components/destination/Mars.jsx
+++ src/components/destination/Mars.jsx
@@ -5,14 +5,15 @@ const Mars = () => {
   return (
     <section className='containerDestination'>
       <article className='containerDestination__left'>
-        <img src={mars} alt="moon" />
+      <span><strong>01</strong> PICK YOUR DESTINATION</span>
+        <img src={mars} alt="mars" />
       </article>
       <article className='containerDestination__right'>
         <nav>
-          <NavLink to="/moon" className='links'>Moon</NavLink>
-          <NavLink to="/mars" className='links'>Mars</NavLink>
-          <NavLink to="/titan" className='links'>Titan</NavLink>
-          <NavLink to="/europa" className='links'>Europa</NavLink>
+          <NavLink to="/moon" className='links'>MOON</NavLink>
+          <NavLink to="/mars" className='links'>MARS</NavLink>
+          <NavLink to="/titan" className='links'>TITÁN</NavLink>
+          <NavLink to="/europa" className='links'>EUROPA</NavLink>
         </nav>    
         <h1>MARS</h1>
         <p>Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!</p>
